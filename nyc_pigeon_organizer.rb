7429a3fcdcd6e6d331f4138963864a2f51bc0691
Nick Corona <coronaisme@gmail.com>
2019-12-05 20:26:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ def nyc_pigeon_organizer(data)
+  updated_data = {}
+  
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        
+
+
+    
 end
