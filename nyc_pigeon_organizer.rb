0ccc49f0d07e23dfe0e49e65df4f71b862b40e48
Nick Corona <coronaisme@gmail.com>
2019-12-05 20:26:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,8 +6,18 @@ def nyc_pigeon_organizer(data)
     value.each do |new_value, names|
       names.each do |name|
         
+        unless updated_data[name]
+          updated_data[name] = {}
+        end
         
-
-
-    
+        unless updated_data[name][key]
+          updated_data[name][key] = []
+        end
+        
+        updated_data[name][key].push(new_value.to_s)
+      end
+    end
+  end
+  updated_data
+end
 end
